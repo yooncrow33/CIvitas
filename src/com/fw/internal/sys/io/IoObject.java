709cb3f3840d3e65d3b0292dbb3e0140aa56291d
yooncrow33 <yooncrow@naver.com>
2026-07-18 20:24:09 +0900

    feat: implement file encryption.

--- src/com/fw/internal/sys/io/IoObject.java
+++ src/com/fw/internal/sys/io/IoObject.java
@@ -1,10 +1,15 @@
 package com.fw.internal.sys.io;
 
 import com.fw.internal.utils.InternalUtils;
+import com.fw.main.Core;
 import com.fw.main.api.io.IoInterface;
 
+import javax.crypto.Cipher;
 import javax.swing.*;
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
 import java.util.Properties;
 
 public class IoObject {
@@ -23,13 +28,36 @@ public class IoObject {
         File file = new File(fullPath);
         if (!file.exists()) {
             ioInterface.initLoad(p);
+            return;
         }
-        try (FileInputStream in = new FileInputStream(fullPath)) {
-            p.load(in);
-            ioInterface.load(p);
-        } catch (IOException | NumberFormatException e) {
-            JOptionPane.showMessageDialog(null, "load fail: " + e.getMessage());
+
+        if (Core.get().isUseEncryption()) {
+            try {
+                String encryptedData = Files.readString(Path.of(fullPath));
+                String decryptedData = decrypt(encryptedData);
+
+                if (decryptedData == null) {
+                    throw new Exception("Wrong decryption key or corrupted data.");
+                }
+
+                try (StringReader reader = new StringReader(decryptedData)) {
+                    p.load(reader);
+                }
+                ioInterface.load(p);
+            } catch (IOException | NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "load fail: " + e.getMessage());
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            try (FileInputStream in = new FileInputStream(fullPath)) {
+                p.load(in);
+                ioInterface.load(p);
+            } catch (IOException | NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "load fail: " + e.getMessage());
+            }
         }
+
     }
 
     public void internalSave() {
@@ -40,10 +68,43 @@ public class IoObject {
         File file = new File(fullPath);
         file.getParentFile().mkdirs();
 
-        try (FileOutputStream out = new FileOutputStream(fullPath)) {
-            props.store(out, "Data");
-        } catch (IOException e) {
-            JOptionPane.showMessageDialog(null, "save fail: " + e.getMessage());
+        if (Core.get().isUseEncryption()) {
+            try {
+                StringWriter writer = new StringWriter();
+                props.store(writer, "Data");
+
+                String encryptedData = encrypt(writer.toString());
+                if (encryptedData == null) {
+                    throw new IOException("Encryption failed.");
+                }
+
+                Files.writeString(Path.of(fullPath), encryptedData);
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "save fail: " + e.getMessage());
+            }
+        } else {
+            try (FileOutputStream out = new FileOutputStream(fullPath)) {
+                props.store(out, "Data");
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(null, "save fail: " + e.getMessage());
+            }
         }
+
+    }
+
+    private String encrypt(String strToEncrypt) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.ENCRYPT_MODE, Core.get().encryptionKey);
+            return Base64.getEncoder().encodeToString(cipher.doFinal(strToEncrypt.getBytes()));
+        } catch (Exception e) { return null; }
+    }
+
+    private String decrypt(String strToDecrypt) {
+        try {
+            Cipher cipher = Cipher.getInstance("AES");
+            cipher.init(Cipher.DECRYPT_MODE, Core.get().encryptionKey);
+            return new String(cipher.doFinal(Base64.getDecoder().decode(strToDecrypt)));
+        } catch (Exception e) { return null; }
     }
 }
--- src/com/fw/main/Config.java
+++ src/com/fw/main/Config.java
@@ -1,20 +1,33 @@
 package com.fw.main;
 
+import javax.crypto.spec.SecretKeySpec;
+
 public class Config {
     final String projectName;
     int initWindowWidth;
     int initWindowHeight;
     boolean useKoreanModule;
+    boolean useEncryption;
+    String secretKey = null;
     public String getProjectName() { return projectName; }
     public int getInitWindowWidth() { return initWindowWidth; }
     public int getInitWindowHeight() { return initWindowHeight; }
     public boolean isUseKoreanModule() {return useKoreanModule; }
+    public boolean isUseEncryption() {return useEncryption; }
+    public final SecretKeySpec encryptionKey;
 
     Config(Config.Builder builder) {
         this.projectName = builder.projectName;
         this.initWindowWidth = builder.initWindowWidth;
         this.initWindowHeight = builder.initWindowHeight;
         this.useKoreanModule = builder.useKoreanModule;
+        this.useEncryption = builder.useEncryption;
+        this.secretKey = builder.secretKey;
+        if (secretKey.length() != 16) {
+            System.err.println("Encryption Key is not 16-digit!");
+            encryptionKey = new SecretKeySpec(this.secretKey.getBytes(), "AES");
+            System.exit(0);
+        } else { encryptionKey = new SecretKeySpec("qazwsxedcrfv".getBytes(), "AES"); }
     }
 
     public static class Builder {
@@ -22,6 +35,8 @@ public class Config {
         int initWindowWidth = 1200;
         int initWindowHeight = 300;
         boolean useKoreanModule = false;
+        boolean useEncryption;
+        String secretKey = null;
         public Builder(String projectName) {
             this.projectName = projectName;
         }
@@ -36,6 +51,17 @@ public class Config {
         }
         public Builder setUseKoreanModule(boolean bool) {
             useKoreanModule = bool;
+            return this;
+        }
+        public Builder setUseEncryption(boolean bool) {
+            useEncryption = bool;
+            return this;
+        }
+        /**
+         * a 16-digit character
+         */
+        public Builder setEncryptionKey(String secretKey) {
+            this.secretKey = secretKey;
             return this;
         }
         public Config build() {
